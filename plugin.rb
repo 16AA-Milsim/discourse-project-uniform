f42255abe6c9315c17f0dc2144a2c7e6a765e702
Darojax <daniel.j.frederiksen@gmail.com>
2025-09-26 15:07:09 +0200

    v0.8.0

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-project-uniform
 # about: Discourse Project Uniform
-# version: 0.7.1
+# version: 0.8.0
 # authors: ChatGPT & Darojax
 # url: https://github.com/16AA-Milsim/discourse-project-uniform
 
