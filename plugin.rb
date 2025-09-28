7e32dd0a3e6173afb9f517eb8249498a4a1d2815
Darojax <daniel.j.frederiksen@gmail.com>
2025-09-28 04:29:44 +0200

    v0.9.4 - Another attempt at cache fix

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-project-uniform
 # about: Discourse Project Uniform
-# version: 0.9.3
+# version: 0.9.4
 # authors: OpenAI & Darojax
 # url: https://github.com/16AA-Milsim/discourse-project-uniform
 
