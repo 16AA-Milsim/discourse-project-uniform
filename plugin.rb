51aaa911a072e265ad8acfac93cbe4e8433dc1df
Darojax <daniel.j.frederiksen@gmail.com>
2025-10-07 03:16:31 +0200

    v0.10.0 - Add Recruit GFX and logic

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-project-uniform
 # about: Discourse Project Uniform
-# version: 0.9.8
+# version: 0.10.0
 # authors: OpenAI & Darojax
 # url: https://github.com/16AA-Milsim/discourse-project-uniform
 
